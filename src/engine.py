3fa8ee3a4a8c7636350e92b3a9b7f1d4709a2cc1
tralfaz <bhaler@gmail.com>
2019-06-22 10:06:32 -0700

    end of Tutorial 3 Dungeon generation Rect class

--- src/engine.py
+++ src/engine.py
@@ -15,6 +15,10 @@ def main():
     map_width = 80
     map_height = 45
     
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+    
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
@@ -23,15 +27,17 @@ def main():
     player = Entity(x=screen_width // 2, y=screen_height // 2, char='@', color=libtcod.white)
     npc = Entity(x=screen_width // 2 - 5, y=screen_height // 2, char='@', color=libtcod.yellow)
     entities = [npc, player]
-
+    
     libtcod.console_set_custom_font(fontFile='images/arial10x10.png',
                                     flags=libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     
     libtcod.console_init_root(w=screen_width, h=screen_height, title='Hero Party', fullscreen=False)
     
     con = libtcod.console_new(w=screen_width, h=screen_height)
-
-    game_map = GameMap(map_width, map_height)
+    
+    game_map = GameMap(width=map_width, height=map_height)
+    game_map.make_map(max_rooms=max_rooms, room_min_size=room_min_size, room_max_size=room_max_size,
+                      map_width=map_width, map_height=map_height, player=player)
     
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -45,7 +51,7 @@ def main():
         libtcod.console_flush()
         
         clear_all(con=con, entities=entities)
-
+        
         action = handle_keys(key=key)
         
         move = action.get('move')
